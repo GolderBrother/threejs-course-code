9ade69f119e0ff31b24331a31fe5adc17d840742
James <jamesezhang@tencent.com>
2025-04-20 22:01:26 +0800

    feat: 3d-music-player-2

--- 3d-music-player/src/main.ts
+++ 3d-music-player/src/main.ts
@@ -3,20 +3,25 @@ import * as THREE from 'three';
 import {
   OrbitControls
 } from 'three/addons/controls/OrbitControls.js';
-import player from './player.js';
+import player from './player.ts';
+import analyser from './analyser.ts';
+import { chunk, map, sum } from 'lodash-es';
 
 const listener = new THREE.AudioListener();
-const audio = new THREE.Audio( listener );
+const audio = new THREE.Audio(listener);
 
 const loader = new THREE.AudioLoader();
 // 加上 Audio 加载了音乐
-loader.load('./superman.mp3', function ( buffer ) {
-  audio.setBuffer( buffer );
+loader.load('./superman.mp3', function (buffer) {
+  audio.setBuffer(buffer);
   audio.autoplay = false;
 });
 
 const scene = new THREE.Scene();
 scene.add(player);
+scene.add(analyser);
+player.position.x = 800;
+player.position.z = 600;
 
 const directionLight = new THREE.DirectionalLight(0xffffff, 2);
 directionLight.position.set(500, 400, 300);
@@ -32,7 +37,8 @@ const helper = new THREE.AxesHelper(500);
 scene.add(helper);
 
 const camera = new THREE.PerspectiveCamera(60, width / height, 0.1, 10000);
-camera.position.set(500, 600, 800);
+// camera.position.set(500, 600, 800);
+camera.position.set(0, 800, 1500);
 camera.lookAt(0, 0, 0);
 
 const renderer = new THREE.WebGLRenderer({
@@ -40,7 +46,22 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(width, height)
 
+const audioAnalyser = new THREE.AudioAnalyser(audio);
+function updateHeight() {
+  const frequencyData = audioAnalyser.getFrequencyData();
+
+  const sumArr = map(chunk(frequencyData, 50), (arr) => {
+    return sum(arr);
+  });
+
+  for (let i = 0; i < analyser.children.length; i++) {
+    const mesh = analyser.children[i];
+    const height = sumArr[i] / 4000;
+    mesh.scale.z = height;
+  }
+}
 function render() {
+  updateHeight()
   renderer.render(scene, camera);
   requestAnimationFrame(render);
 }
@@ -63,24 +84,24 @@ renderer.domElement.addEventListener('click', (e) => {
 
   const intersections = rayCaster.intersectObjects(player.children);
 
-  if(intersections.length) {
+  if (intersections.length) {
     const obj = intersections[0].object.target;
-    if(obj) {
-      if(obj.name === 'playBtn') {
+    if (obj) {
+      if (obj.name === 'playBtn') {
         // 点击按钮的时候让音乐播放暂停
         if (pauseBtn) {
           obj.scale.y = 0.6;
-          obj.position.y = -80 * 0.4/2;
-          
+          obj.position.y = -80 * 0.4 / 2;
+
           pauseBtn.scale.y = 1;
           pauseBtn.position.y = 0;
           audio.play()
         }
-      } else if(obj.name === 'pauseBtn') {
+      } else if (obj.name === 'pauseBtn') {
         if (playerBtn) {
           obj.scale.y = 0.6;
-          obj.position.y = -80 * 0.4/2;
-      
+          obj.position.y = -80 * 0.4 / 2;
+
           playerBtn.scale.y = 1;
           playerBtn.position.y = 0;
           audio.pause()
